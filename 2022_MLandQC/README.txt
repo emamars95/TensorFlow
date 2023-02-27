31b7e96cc71dcfc5d4a7c85f9b3a02762995ba60
Emanuele Marsili <emanuele.marsili.siena@gmail.com>
2023-02-27 19:05:17 +0000

    Upload new tutorials

--- 2022_MLandQC/README.txt
+++
@@ -1,2 +0,0 @@
-http://mlatom.com/tutorial-mlqcdyn22/
-https://github.com/mgastegger/CECAM_2022_colab_PS4
--- PyTorch/Tensor.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-print ("ciao")
-mytensor = torch.empty(3,4)
-print(type(mytensor))
-print(mytensor)
